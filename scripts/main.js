0d7055710137e27f92be059fbcfba58bdc13ff52
Pedro Bortolini <pedrowbortolini@gmail.com>
2025-09-28 13:47:31 -0300

    fix: fixed issues in the API

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ AOS.init({
 
 async function updateVisitorCount() {
     try {
-        const response = await fetch('https://vqb0v0vgj0.execute-api.us-east-1.amazonaws.com/visitors', {
+        const response = await fetch('https://ekj79emjk5.execute-api.us-east-1.amazonaws.com/visitors', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
